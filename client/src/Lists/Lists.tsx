0efcdff806d2025a924f8db2736b8e126c0836d5
Tasha C <tasha.s.chin@gmail.com>
2021-04-14 16:11:21 -0700

    Initialize dependabot/npm_and_yarn/server/yargs-parser-13.1.2

---
+++ client/src/Lists/Lists.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { IoIosRemoveCircleOutline, IoMdCreate } from "react-icons/io";
+import '../App.css';
+
+type ListsProps = {
+  lists: {guid: string, name: string}[],
+  type: string,
+  clickHandlerDelete: any
+}
+const Lists = (props: ListsProps) => {
+  const {lists, type, clickHandlerDelete} = props;
+  return (
+    <ul className="list-group list-no-style">
+      {lists.map(list => {
+        let editListLink;
+        if (list) {
+          if (type === "shopping") {
+            editListLink = `/shoppingLists/${list.guid}/edit`;
+          } else if (type === "template") {
+            editListLink = `/settings/listTemplatesEdit/${list.guid}`;
+          }
+          return (
+            <li key={list.guid}>
+              <Link to={editListLink}><span className="list-name">{list.name}</span></Link>
+              <Link to={editListLink}><IoMdCreate className="list-item-icon" /></Link>
+              <IoIosRemoveCircleOutline className="list-item-icon" id={list.guid} onClick={(ev) => (clickHandlerDelete(ev))} />
+            </li>
+          )
+        }
+        if (!list) {
+          return (null)
+        }
+      })}
+    </ul>
+  )
+}
+
+export default Lists;
