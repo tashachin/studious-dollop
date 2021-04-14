010ce278ebe0330f1a9a1a4c1695eba6e61d4d17
Tasha C <tasha.s.chin@gmail.com>
2021-04-14 16:10:50 -0700

    Initialize 26_nav

---
+++ client/src/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './Home';
+import Root from './Root';
+import './App.css';
+import Header from './App/Header/Header';
+//import Footer from './App/Footer/Footer'
+import ListTemplates from './ListTemplates/ListTemplates';
+import ListTemplateDetailForm from './ListTemplates/ListTemplateDetailForm';
+//import ListTemplateDetail from './ListTemplates/ListTemplateDetail';
+import useListTemplates from './utils/hooks/useListTemplates';
+import config from './config';
+
+function App() {
+
+  const {listTemplates, updateListTemplates, removeListTemplates} = useListTemplates();
+
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+          <Route path='/' exact component={Root}/>
+          <Route exact path="/settings/listTemplates" 
+            render={(props) => (<ListTemplates {...props} updateListTemplates={updateListTemplates}
+              removeListTemplates={removeListTemplates} listTemplates={listTemplates}
+            />
+          )} />
+          <Route exact path="/settings/listTemplatesNew"
+            render={(props) => (<ListTemplateDetailForm {...props} mode="add" updateListTemplates={updateListTemplates}
+            />
+          )} />
+          <Route exact path="/settings/listTemplatesEdit/:listTemplateGuid"
+            render={({match}) => (<ListTemplateDetailForm mode="edit" updateListTemplates={updateListTemplates}
+              listTemplateGuid={match.params.listTemplateGuid}
+            />
+          )} />
+      </div>
+    </Router>
+  )
+}
+
+export default App;
+
+//              listTemplates={listTemplates}
+// listTemplates={listTemplates}
---
+++ client/src/utils/http_requests.js
@@ -0,0 +1,108 @@
+import { getCookieStr } from './utils';
+
+const API_ROOT = process.env.API_ROOT;
+//URL MIDDLE
+const API_LIST_MIDDLE = '/list';
+const API_AUTH_MIDDLE = '/auth';
+//URL SUFFIXES
+const CREATE_LIST_SUFFIX = '/add';
+const GET_SHOPPING_LISTS_SUFFIX = '/shoppingLists';
+const GET_TEMPLATE_LISTS_SUFFIX = '/templateLists';
+const LIST_DETAIL_SUFFIX = '/listDetail';
+const AUTH_REGISTER_SUFFIX = '/register';
+const AUTH_LOGIN_SUFFIX = '/login';
+const AUTH_LOGOUT_SUFFIX = '/logout';
+const AUTH_AUTHENTICATED_SUFFIX = '/authenticated';
+
+let cookieStr = getCookieStr();
+
+const requests = {
+  get: (url) => {
+    return new Promise((resolve, reject) => {
+      fetch(url)
+        .then(resp => resolve(resp.json()))
+        .catch(err => reject(err));
+      //console.error('fetch error', err));
+    });
+  },
+  post: (url, body) => {
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        credentials: 'same-origin',
+        body: JSON.stringify(body),
+      })
+        .then(resp => resolve(resp.json()))
+        .catch(err => reject(err));
+    });
+  },
+  put: (url, body) => {
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        credentials: 'same-origin',
+        body: JSON.stringify(body),
+      })
+        .then(resp => resolve(resp.json()))
+        .catch(err => reject(err));
+    });
+  },
+  delete: (url) => {
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method: 'DELETE',
+        credentials: 'same-origin'
+      })
+        .then(resp => resolve(resp.json()))
+        .catch(err => reject(err));
+    });
+  }
+};
+
+const Lists = {
+  getAllShoppingLists: () => {
+    return requests.post(`${API_ROOT}${API_LIST_MIDDLE}${GET_SHOPPING_LISTS_SUFFIX}`, { cookieStr });
+  },
+  getAllTemplateLists: () => {
+    return requests.post(`${API_ROOT}${API_LIST_MIDDLE}${GET_TEMPLATE_LISTS_SUFFIX}`, { cookieStr });
+  },
+  getTemplateList: (guid) => {
+    return requests.post(`${API_ROOT}${API_LIST_MIDDLE}${LIST_DETAIL_SUFFIX}/${guid}`, { cookieStr });
+  },
+  postTemplateList: (list) => {
+    return requests.post(`${API_ROOT}${API_LIST_MIDDLE}${CREATE_LIST_SUFFIX}`, { ...list, cookieStr });
+  },
+  putTemplateList: (list) => {
+    return requests.put(`${API_ROOT}${API_LIST_MIDDLE}${LIST_DETAIL_SUFFIX}/${list.guid}`, { ...list, cookieStr });
+  },
+  deleteTemplateList: (guid) => {
+    return requests.delete(`${API_ROOT}${API_LIST_MIDDLE}${LIST_DETAIL_SUFFIX}/${guid}`);
+  }
+};
+
+const Auth = {
+  postRegister: (email, password) => {
+    return requests.post(`${API_ROOT}${API_AUTH_MIDDLE}${AUTH_REGISTER_SUFFIX}`, {email, password});
+  },
+  postLogin: (email, password) => {
+    return requests.post(`${API_ROOT}${API_AUTH_MIDDLE}${AUTH_LOGIN_SUFFIX}`, {email, password});
+  },
+  postLogout: (cookie) => {
+    return requests.post(`${API_ROOT}${API_AUTH_MIDDLE}${AUTH_LOGOUT_SUFFIX}`, { cookie });
+  }
+  ,
+  postAuthenticated: (cookie) => {
+    return requests.post(`${API_ROOT}${API_AUTH_MIDDLE}${AUTH_AUTHENTICATED_SUFFIX}`, { cookie });
+  }
+}
+
+export default {
+  Lists,
+  Auth,
+};
